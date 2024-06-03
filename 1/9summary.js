b6f71d1501c1efaf7e86ac0cc916f07c2d4f8273
gorakhnath18 <137605216+gorakhnath18@users.noreply.github.com>
2024-06-03 13:41:47 +0000

    9summary

---
+++ 1/9summary.js
@@ -0,0 +1,51 @@
+//primitive
+
+// 7 cztegories
+//1 string
+// number
+// boolean
+// null
+// undefined
+//symbol
+//bigint
+
+const score =100
+const scorevalue =100.3
+const isloggedin =true
+
+const temp = null
+
+let email;
+const id =symbol('123')
+
+//console.log(id);
+
+const big =251221152225222222222222222n
+
+
+
+
+
+
+
+
+
+
+// refrence  or non primitive
+
+//arrays
+//objects
+//functions
+
+
+const heros=["shaktiman","nagraj"];
+ let myobj={
+    name:"ram",
+    age:21
+
+}
+const myfunction=function(){
+    console.log("hello wrld");
+}
+//console.log( typeof big);
+console.log(typeof scorevalue);
