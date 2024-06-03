b6a5a477185bdf59beec949a12a121ab5313e3d2
gorakhnath18 <137605216+gorakhnath18@users.noreply.github.com>
2024-06-03 04:38:05 +0000

    3

---
+++ 1/3datatypes.js
@@ -0,0 +1,32 @@
+"use strict";//treat all js code as neweer version
+
+//alert(3+3) we are using node js not browser
+
+console.log(3+3); console.log("ran");
+
+//it can be runnning bt its not good for code readinlity
+
+
+let name="hare krishna"//string datatype
+let age= 19 //number
+let isloggedin= false//boolean data type
+// primitive datatypes
+//number=>2 to power 53(range)
+//bigint  = for big number
+//string="  "
+//boolean (yes or no)
+//null=> standalone (represention for empty value)not undefined
+//undefined=> not assigned value
+// symbol=> for findig uniqueness
+
+
+
+//object
+
+console.log(typeof "ram");
+console.log(typeof 23);
+console.log(typeof null );
+console.log(typeof undefined);
+ //learn specivication on mdn
+
+
---
+++ 1/3variables.js
@@ -0,0 +1,15 @@
+const accountId= 144553
+let accountemail="gorakhnathx34@gmail.accountemail"
+var accountpassword = "1234560"
+accountcity ="jaipur"
+let accounstate="ind"
+
+//accountId = 2//not allowed
+console.log(accountId);
+accountemail ="abc@gmail.com"
+accountpassword= "4525"
+accountcity="delhi"
+accounstate="india"
+console.table([accountId,accountemail,accountpassword,accountcity,accountstate])
+/*prefer noyt to use var*/
+console.log(accounstate);
