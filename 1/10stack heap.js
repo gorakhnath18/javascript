a50cc9bf9693471c6da24b71e7ec919718f01f86
gorakhnath18 <137605216+gorakhnath18@users.noreply.github.com>
2024-06-03 13:54:11 +0000

    10stack&heap

---
+++ 1/10stack heap.js
@@ -0,0 +1,23 @@
+//stack(primitive),heap(non primitive)
+let myname ="gorakhnath"
+//console.log(myname);
+
+let anothername= myname
+anothername= "ram"
+
+console.log(myname);
+console.log(anothername);
+
+
+let user={
+    email:"gorakhnathx34@gmail.com",
+    upi:"85ab56@ybl"
+}
+let user2=user
+user2.email="abc.gmail.com"
+console.log(user.email);
+console.log(user2.email);
+
+
+
+
