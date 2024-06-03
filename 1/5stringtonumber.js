c3a5375c31a8d2e4db2fd590ee23a3f669084413
gorakhnath18 <137605216+gorakhnath18@users.noreply.github.com>
2024-06-03 05:08:32 +0000

    5string conversion

---
+++ 1/5stringtonumber.js
@@ -0,0 +1,61 @@
+let somenumber=33
+
+
+let stringnumber=String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+//**************operations*************************** */
+
+let value=3
+let negvalve=-value
+console.log(negvalve);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%3);
+
+
+let str1= "hello"
+let str2 =" ji" //adding of strings
+let str3 =str1+str2
+console.log(str3);
+
+
+//where problrm occurs
+console.log("1"+2);
+console.log(1+"2");
+//what if both are string
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+//
+
+//check at mdn 
+//use parenthesis
+
+
+console.log(+true);
+
+
+/********************assignment operater  ***** */
+let num1,num2,num3
+num1=num2=num3= 2 + 2
+let gamecounter =100
+gamecounter++;
+console.log(gamecounter);
+
+
+//link to study
+//operation
+//   https://tc39.es/ecma262/multipage/abstract-operations.html#sec-abstract-operations 
+
+
+
+
+
+
