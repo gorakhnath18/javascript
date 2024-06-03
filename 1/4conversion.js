1f8b5e6ec64d29eecd3929c2a1fab457b37bfc7d
gorakhnath18 <137605216+gorakhnath18@users.noreply.github.com>
2024-06-03 04:36:17 +0000

    4conversion

---
+++ 1/4conversion.js
@@ -0,0 +1,31 @@
+let score = true
+ 
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+//"33"=33
+//"33abc"=NaN
+//true= 1
+//null=0
+//undefined =NaN
+let isloggedin = "abc"
+let booleanisloggedin= Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+//1=true
+//0= false 
+//" "= false
+//"abc"=true
+
+
+let somenumber=33
+
+
+let stringnumber=String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+//
