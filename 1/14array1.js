2c31af52497b2b2f47a10518fbb05016eaa252b9
gorakhnath18 <137605216+gorakhnath18@users.noreply.github.com>
2024-06-05 04:34:48 +0000

    array

---
+++ 1/14array1.js
@@ -0,0 +1,36 @@
+//araay
+
+const arr =[4,1,2,3,4]
+//console.log(arr);
+const hero =["thor","captian america"]
+
+
+console.log(hero[0]);
+
+ 
+//array methods
+//push function is ussed to add any value
+arr.push(6)
+console.log(arr);
+hero.push("loki")
+console.log(hero);
+
+//pop function
+arr.pop(1)
+console.log(arr);
+hero.pop("thor")
+console.log(hero);
+arr.unshift(0)
+arr.unshift(9)
+console.log(arr);
+arr.shift
+console.log(arr);
+
+//pop function removes the last  added alement of the array
+console.log(arr.includes(9));
+console.log(arr.indexOf(9));
+
+//
+const arr1 =arr.join()
+ //slice ,splice
+console.log("A",arr);
